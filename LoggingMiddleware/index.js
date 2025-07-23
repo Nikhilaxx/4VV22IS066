8d68d3a27f41ef54693e902eaaa6c1e1de33f02f
nikhilaxx <nikhish07@gmail.com>
2025-07-23 12:20:48 +0530

    added url logic

--- LoggingMiddleware/index.js
+++ LoggingMiddleware/index.js
@@ -37,3 +37,60 @@ app.use((req,res,next)=>{
   next()
 })
 
+const shortUrls={}
+app.post('/shorturls',(req,res)=>{
+  const{url,validity,shortcode}=req.body
+  
+
+  let code=shortcode
+  if(shortUrls[code]){
+    return res.status(409).json({error:'Shortcode already exists'})
+  }
+
+  const now=new Date()
+  const expiresAt=new Date(now.getTime()+(validity||30)*60000)
+
+  shortUrls[code]={
+    originalUrl:url,
+    createdAt:now,
+    expiresAt,
+  }
+
+  res.json({
+    shortLink:`http://localhost:${PORT}/${code}`,
+    expiresAt:expiresAt.toISOString()
+  })
+})
+
+app.get('/:shortcode',(req,res)=>{
+  const code=req.params.shortcode
+  const data=shortUrls[code]
+
+  if(!data){
+    return res.status(404).json({error:'Shortcode not found'})
+  }
+
+  if(new Date()>data.expiresAt){
+    return res.status(410).json({error:'Short link expired'})
+  }
+})
+
+app.get('/shorturls/:shortcode',(req,res)=>{
+  const code=req.params.shortcode
+  const data=shortUrls[code]
+
+  if(!data){
+    return res.status(404).json({error:'Shortcode not found'})
+  }
+
+  res.json({
+    originalUrl:data.originalUrl,
+    createdAt:data.createdAt,
+    expiresAt:data.expiresAt,
+    
+  })
+})
+
+app.listen(PORT,()=>{
+  console.log(`Server running at http://localhost:${PORT}`)
+})
