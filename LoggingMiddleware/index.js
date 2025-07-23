cb938f9efb044f946d5c0e1ad13602d1a30858b1
nikhilaxx <nikhish07@gmail.com>
2025-07-23 12:08:08 +0530

    added Logging middleware

--- LoggingMiddleware/index.js
+++ LoggingMiddleware/index.js
@@ -1,11 +1,39 @@
-const express=require('express');
-const app=express();
-const PORT=3000;
+const express=require('express')
+const app=express()
+const bodyParser=require('body-parser')
+const http=require('http')
 
-app.get('/',(req,res)=>{
-  res.send('Server is up');
-});
+const PORT=3000
+app.use(bodyParser.json())
 
-app.listen(PORT,()=>{
-  console.log(`Server runniing at http://localhost:${PORT}`);
-});
+function Log(stack,level,pkg,message){
+  const data=JSON.stringify({
+    stack,level,package:pkg,message
+  })
+
+  const options={
+    hostname:'localhost',
+    port:9000,
+    path:'/logs',
+    method:'POST',
+    headers:{
+      'Content-Type':'application/json',
+      'Content-Length':data.length
+    }
+  }
+
+  const req=http.request(options,res=>{
+    res.on('data',()=>{})
+  })
+
+  req.on('error',()=>{})
+  req.write(data)
+  req.end()
+}
+
+app.use((req,res,next)=>{
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
+  Log("backend","info","middleware",`${req.method} ${req.url}`)
+  next()
+})
+
